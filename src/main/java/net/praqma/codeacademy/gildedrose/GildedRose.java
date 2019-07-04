156f3af004f5dbd931c2fd709a12117ffd35e921
FelixBoelle <felixtim.boelle@gmail.com>
2019-07-04 11:12:45 +0200

    added conjured mana cake item in here

--- src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
+++ src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
@@ -19,7 +19,11 @@ class GildedRose {
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
+                    // adding new lines here for conjured items
+		    if (items[i].name.equals("Conjured Mana Cake")) {
+			items[i].quality = items[i].quality + 1; // degrades twice as fast
+		    }
+		    
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
